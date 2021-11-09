b0f43d85bb17a8920039dd9dfafc4ce0b9971c2a
Dmitrii <zeionara@gmail.com>
2021-11-09 18:02:36 +0300

    Added support for set-bern function form the meager lib

--- lib/meager_adapter.ex
+++ lib/meager_adapter.ex
@@ -9,8 +9,16 @@ defmodule Meager do
     raise "NIF set_in_path/4 not implemented"
   end
 
-  def set_in_path(path, as_tsv) do
+  def set_input_path(path, as_tsv) do
     set_in_path(String.to_charlist(path), as_tsv, String.length(path), String.length(Atom.to_string(as_tsv)))
+  end
+
+  def set_bern(_a) do
+    raise "NIF set_bern/1 not implemented"
+  end
+
+  def set_bern_flag(value) do
+    set_bern(if value, do: 1, else: 0)
   end
 end
 
