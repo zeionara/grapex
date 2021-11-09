b03987ee38b63cd9ed4f782096820d3e75f19868
Dmitrii <zeionara@gmail.com>
2021-11-09 15:40:59 +0300

    Added module serving as a libmeager adapter

--- lib/fast_compare.ex
+++ lib/fast_compare.ex
@@ -1,20 +1,13 @@
 defmodule FastCompare do
-  @on_load :load_nifs
+  # @on_load :load_nifs
 
-  def load_nifs do
-    :erlang.load_nif('/usr/lib/libmeager', 0)
-  end
+  # def load_nifs do
+  #   :erlang.load_nif('/usr/lib/libmeager', 0)
+  # end
 
   def fast_compare(_a, _b) do
     raise "NIF fast_compare/2 not implemented"
   end
 
-  def set_in_path(_a, _b, _c, _d) do
-    raise "NIF set_in_path/4 not implemented"
-  end
-
-  def set_in_path(path, as_tsv) do
-    set_in_path(String.to_charlist(path), as_tsv, String.length(path), String.length(Atom.to_string(as_tsv)))
-  end
 end
 
---
+++ lib/meager_adapter.ex
@@ -0,0 +1,16 @@
+defmodule Meager do
+  @on_load :load_nifs
+
+  def load_nifs do
+    :erlang.load_nif('/usr/lib/libmeager', 0)
+  end
+
+  def set_in_path(_a, _b, _c, _d) do
+    raise "NIF set_in_path/4 not implemented"
+  end
+
+  def set_in_path(path, as_tsv) do
+    set_in_path(String.to_charlist(path), as_tsv, String.length(path), String.length(Atom.to_string(as_tsv)))
+  end
+end
+
