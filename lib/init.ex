b62463fd7cc00fee561882e6e8dce8f9677956df
Dmitrii <zeionara@niuitmo.ru>
2021-11-12 02:03:37 +0300

    Implemented macros for model initializaion

--- lib/init.ex
+++ lib/init.ex
@@ -1,25 +1,62 @@
-defmodule Grapex.Init do
-  import Map
-  
-  @spec set_input_path(map, string) :: map
-  def set_input_path(config, path) do
-    put(config, :input_path, path)
-  end
-    
-  @spec set_input_path(string) :: map
-  def set_input_path(path) do
-    set_input_path(%{}, path)
-  end
+defmodule Grapex.Init.Macros do
 
-  @spec set_n_epochs(map, integer) :: map
-  def set_n_epochs(config, n_epochs) do
-    put(config, :n_epochs, n_epochs)
-  end
-   
-  @spec set_input_path(integer) :: map
-  def set_n_epochs(n_epochs) do
-    set_n_epochs(%{}, n_epochs)
+  defmacro defparam(clause, as: type) do
+    # IO.inspect(clause)
+
+    # quoted = quote do
+    #   if(unquote(clause), do: a = 2)
+    # end
+    function_name = String.to_atom("set_#{clause}")
+    
+    quoted = quote do
+
+      @spec unquote(function_name)(map, unquote(type)) :: map
+      def unquote(function_name)(config, value) do
+        put(config, unquote(clause), value)
+      end
+
+      @spec unquote(function_name)(unquote(type)) :: map
+      def unquote(function_name)(value) do
+        unquote(function_name)(%{}, value)
+      end
+    end
+
+    # IO.inspect(quoted)
+    quoted
   end
 
 end
+
+defmodule Grapex.Init do
+  import Map
+  import Grapex.Init.Macros
+
+  # @spec set_input_path(map, String.t) :: map
+  # def set_input_path(config, path) do
+  #   put(config, :input_path, path)
+  # end
+  #   
+  # @spec set_input_path(String.t) :: map
+  # def set_input_path(path) do
+  #   set_input_path(%{}, path)
+  # end
+
+  # @spec set_n_epochs(map, integer) :: map
+  # def set_n_epochs(config, n_epochs) do
+  #   put(config, :n_epochs, n_epochs)
+  # end
+  #  
+  # @spec set_input_path(integer) :: map
+  # def set_n_epochs(n_epochs) do
+  #   set_n_epochs(%{}, n_epochs)
+  # end
+
+  # defparam :foo, as: integer
+  
+  defparam :input_path, as: String.t
+  defparam :n_epochs, as: integer
+  defparam :n_batches, as: integer
+
+end
+
 
