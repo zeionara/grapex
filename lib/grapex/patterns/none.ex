cf1d50cb7aad41486b9975fba7e9f4b11c9e4b1f
Dmitrii <zeionara@gmail.com>
2022-01-21 14:34:12 +0300

    Added none pattern implementation

---
+++ lib/grapex/patterns/none.ex
@@ -0,0 +1,34 @@
+defmodule Grapex.Patterns.None do
+  defstruct [:triples]
+end
+
+defimpl Inspect, for: Grapex.Patterns.None do
+  def inspect(occurrence, _opts \\ []) do
+    "\ntriples\n#{TripleOccurrence.describe(occurrence.triples)}\n"
+  end
+end  
+
+defimpl PatternOccurrence, for: Grapex.Patterns.None do
+  def to_tensor(occurrence, %Grapex.Init{entity_negative_rate: entity_negative_rate, relation_negative_rate: relation_negative_rate} = params, opts \\ []) do # , batch_size: batch_size
+    n_positive_iterations = entity_negative_rate + relation_negative_rate
+
+    result = PatternOccurrence.to_tensor(occurrence.triples, params)
+
+    make_true_label = Keyword.get(opts, :make_true_label, nil)
+
+    {_, batch_size, _} = Nx.shape(result.entities)
+    
+    unless make_true_label == nil do
+      # Map.put(result, :true_labels, Nx.tensor(for _ <- 1..(batch_size * n_positive_iterations) do [0.0] end))
+      Map.put(result, :true_labels, Nx.tensor(for _ <- 1..(batch_size * n_positive_iterations) do [make_true_label.()] end))
+    else
+      result
+    end
+    # if with_true_labels do
+    #   Map.put(result, :true_labels, Nx.tensor(for _ <- 1..(batch_size * n_positive_iterations) do [0.0] end))
+    # else
+    #   result
+    # end
+  end
+end
+
