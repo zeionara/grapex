7a97bedec13a65b2589f385ee1eb9e26ea751ec9
zeio <zeionara@gmail.com>
2022-11-09 17:21:43 +0300

    Increased entity-negative-rate

--- lib/grapex/init.ex
+++ lib/grapex/init.ex
@@ -26,7 +26,7 @@ defmodule Grapex.Init do
     :input_path, :model, :batch_size, :input_size, :output_path, :import_path, :seed, :min_delta, :patience, :n_export_steps, :model_impl,
     :relation_dimension, :entity_dimension, 
     :trainer, :reverse, :tester, :max_n_test_triples, :n_batches, # , :n_test_triples
-    n_epochs: 10, entity_negative_rate: 1, relation_negative_rate: 0, as_tsv: false, remove: false, verbose: false, is_imported: false, validate: false, bern: false,
+    n_epochs: 10, entity_negative_rate: 25, relation_negative_rate: 0, as_tsv: false, remove: false, verbose: false, is_imported: false, validate: false, bern: false,
     hidden_size: 10, n_workers: 8, optimizer: :sgd, task: :link_prediction,
     margin: 5.0, alpha: 0.1, lambda: 0.1, compiler: :default, compiler_impl: Nx.Defn.Evaluator, enable_bias: true, enable_filters: false, drop_duplicates_during_filtration: true
   ]
