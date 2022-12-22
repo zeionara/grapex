74e8a48d4ebb691fe77a783f58adc5c0b5516461
zeio <zeionara@gmail.com>
2022-12-23 02:07:43 +0300

    Started implementing config importer from yaml file

--- lib/grapex/config.ex
+++ lib/grapex/config.ex
@@ -11,6 +11,10 @@ defmodule Grapex.Config do
     :checkpoint,
   ]
 
+  @key_handlers %{
+    :model => &Grapex.Model.import/1
+  }
+
   @keys @enforce_keys
 
   defstruct @keys
@@ -22,11 +26,15 @@ defmodule Grapex.Config do
       case Map.get(config, key) do
         nil -> nil # raise "No key #{key} in file #{path}"
         values ->
-          short_module_name =
-            key
-            |> Atom.to_string
-            |> String.capitalize
-          apply(String.to_existing_atom("Elixir.Grapex.#{short_module_name}"), :import, [values])
+          case @key_handlers[key] do
+            nil -> values
+            handler -> handler.(values)
+          end
+          # short_module_name =
+          #   key
+          #   |> Atom.to_string
+          #   |> String.capitalize
+          # apply(String.to_existing_atom("Elixir.Grapex.#{short_module_name}"), :import, [values])
       end
     end
 
--- lib/grapex/model.ex
+++ lib/grapex/model.ex
@@ -1,3 +1,223 @@
+defmodule Grapex.Model.Config do
+
+  defmacro __using__(opts) do
+    enforced_keys = Keyword.get(opts, :enforced_keys)
+    regular_keys = Keyword.get(opts, :regular_keys)
+    attributes = Keyword.get(opts, :attributes)
+
+    # quoted = quote do
+    #   @foo [2, 3]
+    # end
+
+    # IO.inspect quoted
+
+    # quoted = quote do
+    #   def import(%{:model => model, :hidden_size => hidden_size, :reverse => reverse} = config) do
+    #   # def import(%{:foo => bar} = config) do
+    #     # IO.inspect unquote(params)
+    #     %Grapex.Model{model: model, hidden_size: hidden_size, reverse: reverse} | config
+    #     # %Grapex.Model{foo: bar} | config
+    #   end
+    # end
+
+    # IO.inspect quoted
+
+    # reversed_args = 
+    #   for param <- Enum.reverse enforced_keys do
+    #     # {:%{}, [], [{param, {param, [], Grapex.Model.Config}}]}
+    #     {param, {param, [], Grapex.Model.Config}}
+    #   end
+
+    args = 
+      for param <- enforced_keys do
+        # {:%{}, [], [{param, {param, [], Grapex.Model.Config}}]}
+        {param, {param, [], Grapex.Model.Config}}
+      end
+
+    # IO.inspect args
+
+    quoted =  {:def, [context: Grapex.Model.Config, imports: [{1, Kernel}, {2, Kernel}]],
+     [
+       {:import, [context: Grapex.Model.Config],
+        [
+          {:=, [],
+           [
+             {:%{}, [], args},
+             {:config, [], Grapex.Model.Config}
+           ]}
+        ]},
+       [
+         do: {:|, [],
+          [
+            {:%, [],
+             [
+               {:__aliases__, [alias: false], [:Grapex, :Model]},
+               {:%{}, [], args}
+             ]},
+            {:config, [], Grapex.Model.Config}
+          ]}
+       ]
+     ]}
+
+
+    # updated_head = [
+    #   {:config, [], Grapex.Model.Macros} | reversed_args
+    # ]
+    # |> Enum.reverse
+
+    # updated_head_2 = [
+    #   {:__aliases__, [alias: false], [:Grapex, :Model]} | (reversed_args |> Enum.reverse)
+    # ]
+
+    # quoted = {:def, [context: Grapex.Model.Config, imports: [{1, Kernel}, {2, Kernel}]],
+    #  [
+    #    {:import, [context: Grapex.Model.Config],
+    #     [
+    #       # {:=, [], updated_head}
+    #       {:%, [], updated_head},
+    #       {:config, [], Grapex.Model.Macros}
+    #     ]},
+    #    [
+    #      do: {:|, [],
+    #       [
+    #         {:%, [], updated_head_2},
+    #         {:config, [], Grapex.Model.Config}
+    #       ]}
+    #    ]
+    #   ]
+    # }
+
+    # IO.inspect quoted
+
+    # IO.inspect keys
+    # IO.inspect quoted
+
+    full_quote = {_block, _empty_list, items} = quote do
+
+      # unquote do
+      #   if attributes != nil do
+      #     for {attribute, value} <- attributes do
+      #       [{attribute, [context: Grapex.Model.Config], [value]}]
+      #     end
+      #   end
+      # end
+
+      @enforced_keys unquote(enforced_keys)
+      # @enforced_keyss unquote(enforced_keys)
+      defstruct unquote(regular_keys ++ enforced_keys)
+
+      unquote(quoted)
+
+    end
+
+    full_quote = if attributes != nil do
+      updated_items = for {{attribute, _, _}, value} <- attributes do
+        {:@, [context: Grapex.Model.Config, imports: [{1, Kernel}]],
+        [{attribute, [context: Grapex.Model.Config], [value]}]}
+      end ++ items
+      {:__block__, [], updated_items}
+    else
+      full_quote
+    end
+
+    IO.inspect full_quote
+
+    # {a, b} = 2
+
+    full_quote
+
+    # quoted
+  end
+
+end
+
+
+# defmodule Grapex.Model.Macros do
+# 
+#   use Grapex.Model.Config
+#   alias Grapex.Model.Macros
+# 
+#   defmacro importt() do
+#     quoted = quote do
+#       # def import(%{:model => model, :hidden_size => hidden_size, :reverse => reverse} = config) do
+#       def import(%{:foo => bar} = config) do
+#         # IO.inspect unquote(params)
+#         # %Grapex.Model{model: model, hidden_size: hidden_size, reverse: reverse} | config
+#         %Grapex.Model{foo: bar} | config
+#       end
+#     end
+# 
+#     IO.inspect @enforced_keys
+#     # IO.inspect Grapex.Model.init(1, 2)
+#     # params = Macro.expand(params, __CALLER__)
+# 
+#     # IO.inspect params
+# 
+#     IO.inspect quoted
+# 
+#     [
+#       {
+#         _import,
+#         _context,
+#         [
+#           {
+#             _assignment,
+#             _empty_list,
+#             head
+#           }
+#         ]
+#       } |
+#       _tail
+#     ] =
+#       quoted
+#       |> elem(2)
+# 
+#     updated_head = [
+#       {:config, [], Grapex.Model.Macros} |
+#       for param <- Enum.reverse @enforced_keys do
+#         {:%{}, [], [{param, {param, [], Grapex.Model.Macros}}]}
+#       end
+#     ]
+#     |> Enum.reverse
+#     
+#     # quoted_updated = {
+#     #   :def, [context: Macros, imports: [{1, Kernel}, {2, Kernel}]],
+#     #   [
+#     #     {:import, [context: Grapex.Model.Macros],
+# 
+#     # }
+# 
+#     updated_head |> IO.inspect
+# 
+#     head
+#     |> IO.inspect
+# 
+#     {a, b} = 2
+#     quoted
+#   end
+# 
+# 
+#   defmacro defparam(clause, as: type) do
+#     function_name = String.to_atom("set_#{clause}")
+#     
+#     quoted = quote do
+# 
+#       @spec unquote(function_name)(map, unquote(type)) :: map
+#       def unquote(function_name)(config, value) do
+#         put(config, unquote(clause), value)
+#       end
+# 
+#       @spec unquote(function_name)(unquote(type)) :: map
+#       def unquote(function_name)(value) do
+#         unquote(function_name)(%Grapex.Init{}, value)
+#       end
+#     end
+# 
+#     quoted
+#   end
+# 
+# end
+
 defmodule Grapex.Model do
   
   alias Grapex.Model
@@ -5,30 +225,36 @@ defmodule Grapex.Model do
 
   alias Grapex.Model.Transe
 
+  # import Grapex.Model.Macros
+
+  use Grapex.Model.Config, 
+    enforced_keys: [:model, :hidden_size, :reverse],
+    regular_keys: [:entity_size, :relation_size],
+    attributes: [
+      {valid_models, [:transe]}
+    ]
+
   defmacro __using__(_) do
     quote do
-
-      @valid_models [
-        :transe
-      ]
-
+      @valid_models unquote(@valid_models)
     end
   end
 
-  @enforce_keys [
-    :model,
+  # @enforced_keys [
+  #   :model,
 
-    :hidden_size,
-    :reverse
-  ]
+  #   :hidden_size,
+  #   :reverse
+  # ]
 
-  defstruct [
-    # :n_entities
-    # :n_relations,
-    :entity_size,
-    :relation_size | @enforce_keys
-  ]
+  # @enforce_keys @enforced_keys
 
+  # defstruct [
+  #   # :n_entities
+  #   # :n_relations,
+  #   :entity_size,
+  #   :relation_size | @enforced_keys
+  # ]
 
   def init(
     %Config{
@@ -39,7 +265,7 @@ defmodule Grapex.Model do
       trainer: trainer
     },
     _opts \\ []
-  ) do
+  ) when model_type in @valid_models do
     case model_type do
       :transe -> {Transe.init(model, corpus, trainer, verbose: true), Transe}
       _ -> raise "Unknown model type"
@@ -49,11 +275,13 @@ defmodule Grapex.Model do
   def model | config do
     model
     |> Map.put(:entity_size, Map.get(config, :entity_size))
-    |> Map.put(:relation_size, Map.get(config, :entity_size))
+    |> Map.put(:relation_size, Map.get(config, :relation_size))
   end
 
-  def import(%{:model => model, :hidden_size => hidden_size, :reverse => reverse} = config) do
-    %Model{model: model, hidden_size: hidden_size, reverse: reverse} | config
-  end
+  # importt
+
+  # def import(%{:model => model, :hidden_size => hidden_size, :reverse => reverse} = config) do
+  #   %Model{model: model, hidden_size: hidden_size, reverse: reverse} | config
+  # end
 
 end
